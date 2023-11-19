d9d1954f167e34a2e19c4765ec8311c1a2f5de0e
Roman <13500086+romanzy313@users.noreply.github.com>
2023-11-19 16:19:59 +0100

    better typegen

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import path from "path";
-import { defineConfig } from "vitest/config";
-
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "placeholder-lib-name",
-      fileName: (format) => format == "es" ? `index.js` : `index.${format}.js`,
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'placeholder-lib-name',
+      // fileName: 'index',
+      fileName: (format) =>
+        format == 'es' ? `index.js` : `index.${format}.js`,
     },
     sourcemap: true,
   },
+  plugins: [dts()],
   test: {
-    dir: "src"
-  }
+    dir: 'src',
+  },
 });
